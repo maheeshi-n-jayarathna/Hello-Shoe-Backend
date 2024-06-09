647301f77fc29ac78ac51d550fcc57ac2174d8da
maheeshi-n-jayarathna <maheeshijayarathna@gmail.com>
2024-06-09 06:39:27 +0530

    update

---
+++ src/main/java/lk/ijse/gdse65/AAD_Course_Work/dto/ItemDTO.java
@@ -0,0 +1,14 @@
+package lk.ijse.gdse65.AAD_Course_Work.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ItemDTO {
+    private String item_code;
+    private String item_name;
+    private String item_description;
+}
